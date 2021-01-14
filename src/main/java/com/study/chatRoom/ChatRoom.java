35dca6d2c37ba63c3602e612dee1eeb39c11230e
david <41226237+naruto7th@users.noreply.github.com>
2021-01-14 17:35:18 +0800

    chat-chatRoom前端内容

---
+++ src/main/java/com/study/chatRoom/ChatRoom.java
@@ -0,0 +1,13 @@
+package com.study.chatRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoom {
+
+    public static void main(String[] args) {
+        SpringApplication.run (ChatRoom.class, args);
+    }
+
+}
